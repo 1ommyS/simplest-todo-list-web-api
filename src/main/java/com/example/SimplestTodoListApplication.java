792e7efe9a651d70453e909c5a528f032ed93bd0
1ommy <santiago_bravo@mail.ru>
2023-02-04 10:46:51 +0300

    upd

--- src/main/java/com/example/SimplestTodoListApplication.java
+++ src/main/java/com/example/SimplestTodoListApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SimplestTodoListApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SimplestTodoListApplication.class, args);
     }
